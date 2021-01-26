902cbc3664ccf0763a8ec4275eb6ed1c2bb1a70b
ayyuksel <74163951+ayyuksel@users.noreply.github.com>
2021-01-26 15:24:01 -0500

    movie 1 more change

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
 
-        System.out.println("Hello, B21");
+        System.out.println("Hello, B22");
         //watching more movie
 
     }
